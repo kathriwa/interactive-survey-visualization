5697730ff5ced1809ffc46408daf508bd50d2f87
lukasdiebold <46973162+lukasdiebold@users.noreply.github.com>
2025-02-18 13:11:27 +0100

    Update citation text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,19 @@ import { Button, Card } from 'antd';
 import { CopyOutlined } from '@ant-design/icons';
 
 const Home = () => {
-  const citationText = `@misc{wardatzky2024explanationsservesystematicliterature,
-  title={Whom do Explanations Serve? A Systematic Literature Survey of User Characteristics in Explainable Recommender Systems Evaluation}, 
-  author={Kathrin Wardatzky and Oana Inel and Luca Rossetto and Abraham Bernstein},
-  year={2024},
-  eprint={2412.14193},
-  archivePrefix={arXiv},
-  primaryClass={cs.HC},
-  url={https://arxiv.org/abs/2412.14193}, 
+  const citationText = `@article{10.1145/3716394,
+  author = {Wardatzky, Kathrin and Inel, Oana and Rossetto, Luca and Bernstein, Abraham},
+  title = {Whom do Explanations Serve? A Systematic Literature Survey of User Characteristics in Explainable Recommender Systems Evaluation},
+  year = {2025},
+  publisher = {Association for Computing Machinery},
+  address = {New York, NY, USA},
+  url = {https://doi.org/10.1145/3716394},
+  doi = {10.1145/3716394},
+  abstract = {Adding explanations to recommender systems is said to have multiple benefits, such as increasing user trust or system transparency. Previous work from other application areas suggests that specific user characteristics impact the users’ perception of the explanation. However, we rarely find this type of evaluation for recommender systems explanations. This paper addresses this gap by surveying 124 papers in which recommender systems explanations were evaluated in user studies. We analyzed their participant descriptions and study results where the impact of user characteristics on the explanation effects was measured. Our findings suggest that the results from the surveyed studies predominantly cover specific users who do not necessarily represent the users of recommender systems in the evaluation domain. This may seriously hamper the generalizability of any insights we may gain from current studies on explanations in recommender systems. We further find inconsistencies in the data reporting, which impacts the reproducibility of the reported results. Hence, we recommend actions to move toward a more inclusive and reproducible evaluation.},
+  note = {Just Accepted},
+  journal = {ACM Trans. Recomm. Syst.},
+  month = feb,
+  keywords = {explainable AI, recommender systems, user studies, literature survey}
 }`
   const copyToClipboard = () => {
     navigator.clipboard.writeText(citationText);
@@ -40,7 +45,7 @@ const Home = () => {
           icon={<CopyOutlined />} 
           size={'middle'} 
         />
-        <pre style={{ margin: 0, whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+        <pre style={{ margin: 0, overflowX: 'auto', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
           <code>{citationText}</code>
         </pre>
       </Card>
